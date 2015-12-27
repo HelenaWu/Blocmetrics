1ec625955bba1e605ef716a3ede28571646779ae
Helena Wu <Helena.wu87@gmail.com>
2015-12-27 15:11:27 -0800

    initial commit, simple authentication system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "logged in"
+    else
+      flash[:alert] = "invalid log in credentials"
+      render "new"      
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "successfully logged out"
+  end
+end
+
